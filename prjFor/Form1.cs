2c46fea476efb677ee6ac1543bf98f577560964a
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-07-25 13:38:36 +0800

    upload

--- prjFor/Form1.cs
+++ prjFor/Form1.cs
@@ -6,5 +6,25 @@ namespace prjFor
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                sum += i;
+            }
+            MessageBox.Show("Sum of 1 to 10 is " + sum);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double sum = 0.0;
+            for (double i = 0.5; i <= 5.0; i+=0.5)
+            {
+                sum += i;
+            }
+            MessageBox.Show("Sum of 0.5 to 5.0 is " + sum);
+        }
     }
 }
--- prjFor/obj/Debug/net8.0-windows/prjFor.AssemblyInfo.cs
+++ prjFor/obj/Debug/net8.0-windows/prjFor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prjFor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9ab7b6960535dcf899661757542356049c55ce5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+142d45b9948496823bcce0a03defe33fa57ff308")]
 [assembly: System.Reflection.AssemblyProductAttribute("prjFor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prjFor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
